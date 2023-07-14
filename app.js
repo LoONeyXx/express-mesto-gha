25a3599128ad42d4d46af5caf998ffbbaee8e0a5
Sergey Gromov <looneyxx@yandex.ru>
2023-07-14 20:16:14 +0300

    add Helmet

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 import router from "./routes/index.js";
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(helmet());
+app.disable("x-powered-by");
 app.use((req, res, next) => {
   req.user = {
     _id: "64b11c63933c0199373677ca",
