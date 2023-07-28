503e1c1c87da857e88eb4470a922b82fa5a22839
Sergey Gromov <looneyxx@yandex.ru>
2023-07-28 19:07:02 +0300

    refactoring 2

--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,9 @@ router.post("/signup", userValidatorAuth(), addUser);
 router.post("/signin", userValidatorAuth(), login);
 router.use(auth);
 router.get("/users", getAllUsers);
-router.get("/users/me", userIdValidator(), getUser);
+router.get("/users/me", getUser);
 router.get("/users/:userId", userIdValidator(), getUser);
-router.patch("/users/me", updateUser);
+router.patch("/users/me", userValidatorUpdate(), updateUser);
 router.patch("/users/me/avatar", userValidatorUpdate(), updateUser);
 
 export default router;
