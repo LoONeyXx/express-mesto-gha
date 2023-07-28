e154f7e2dea700d9ab01ec42706ff879e65202c9
Sergey Gromov <looneyxx@yandex.ru>
2023-07-28 13:12:45 +0300

    test

--- utils/utils.js
+++ utils/utils.js
@@ -59,6 +59,9 @@ export const userValidatorAuth = () => celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+    avatar: Joi.string().uri().min(8),
+    name: Joi.string().max(30).min(2),
+    about: Joi.string().max(30).min(2),
   }),
 });
 
